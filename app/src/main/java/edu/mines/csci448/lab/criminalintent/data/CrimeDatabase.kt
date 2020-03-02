1f50aa676745e48b547af2cacb3af46053241d56
Nicholas Wenzel <nwenzel@mines.edu>
2020-03-02 13:26:51 -0700

    Updated database with two new columns

--- app/src/main/java/edu/mines/csci448/lab/criminalintent/data/CrimeDatabase.kt
+++ app/src/main/java/edu/mines/csci448/lab/criminalintent/data/CrimeDatabase.kt
@@ -2,10 +2,12 @@ package edu.mines.csci448.lab.criminalintent.data
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 private const val DATABASE_NAME = "crime-database"
 
-@Database(entities = [Crime :: class], version = 1)
+@Database(entities = [Crime :: class], version = 2)
 @TypeConverters(CrimeTypeConverters :: class)
 
 
@@ -16,9 +18,17 @@ abstract class CrimeDatabase: RoomDatabase() {
     companion object{
         private var instance: CrimeDatabase? = null
 
+        private val migration_1_2 = object : Migration(1,2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE crime ADD COLUMN suspect TEXT DEFAULT NULL")
+                database.execSQL("ALTER TABLE crime ADD COLUMN suspectNumber TEXT DEFAULT NULL")
+            }
+        }
+
         fun getInstance(context: Context): CrimeDatabase {
             return instance ?: let {
-                instance ?: Room.databaseBuilder(context, CrimeDatabase :: class.java, DATABASE_NAME).build()
+                instance ?: Room.databaseBuilder(context, CrimeDatabase :: class.java, DATABASE_NAME).addMigrations(
+                    migration_1_2).build()
             }
         }
     }
