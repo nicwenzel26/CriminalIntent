4fde24f9bf53504e86adadf6e978ff50e09007d6
nwenzel <nwenzel@mymail.mines.edu>
2020-03-05 18:00:45 -0700

    Get persons name from contacts

--- app/src/main/java/edu/mines/csci448/lab/criminalintent/ui/detail/CrimeDetailFragment.kt
+++ app/src/main/java/edu/mines/csci448/lab/criminalintent/ui/detail/CrimeDetailFragment.kt
@@ -1,9 +1,12 @@
 package edu.mines.csci448.lab.criminalintent.ui.detail
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.EXTRA_SUBJECT
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -22,6 +25,7 @@ import java.util.*
 import kotlin.math.log
 
 private const val ARG_CRIME_ID = "crime_id"
+private const val REQUEST_CODE_CONTACT = 1
 
 class CrimeDetailFragment : Fragment() {
 
@@ -32,6 +36,9 @@ class CrimeDetailFragment : Fragment() {
     private lateinit var solvedCheckBox: CheckBox
     private lateinit var crimeDetailViewModel: CrimeDetailViewModel
     private lateinit var crimeReportButton: Button
+    private lateinit var crimeSuspectButton: Button
+
+    private val pickContactIntent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
 
 
     companion object {
@@ -83,6 +90,47 @@ class CrimeDetailFragment : Fragment() {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        } else {
+            if (requestCode == REQUEST_CODE_CONTACT) {
+                if (data == null) {
+                    return
+                }
+                // CrimeDetailFragment.kt where “future code goes here” comment was
+                val contactUri = data.data ?: return
+                // specify which fields you want your query to return values for
+                val queryFields = listOf(ContactsContract.Contacts.DISPLAY_NAME)
+                // perform your query, the contactUri is like a "where" clause
+                val cursor =
+                    requireActivity().contentResolver.query(
+                        contactUri,
+                        queryFields.toTypedArray(),
+                        null,
+                        null,
+                        null
+                    )
+                cursor?.use { contactIter ->
+                    // double check that you got results
+                    if (contactIter.count == 0) {
+                        return
+                    }
+                    // pull out the first column of the first row of data
+                    // that is the contact's name
+                    contactIter.moveToFirst()
+                    val suspect = contactIter.getString(0)
+                    crime.suspect = suspect
+                    // set the crime's suspect field
+                    crimeDetailViewModel.saveCrime(crime)// save the crime
+                    crimeSuspectButton.text = suspect
+                    // change the button text
+                }
+            }
+        }
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -95,6 +143,11 @@ class CrimeDetailFragment : Fragment() {
         dateButton = view.findViewById(R.id.crime_date_button) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved_checkbox) as CheckBox
         crimeReportButton = view.findViewById(R.id.crime_report_button) as Button
+        crimeSuspectButton = view.findViewById(R.id.crime_suspect_button) as Button
+
+        crimeSuspectButton.setOnClickListener { startActivityForResult(pickContactIntent, REQUEST_CODE_CONTACT) }
+
+        crimeSuspectButton.isEnabled = requireActivity().packageManager.resolveActivity(pickContactIntent, PackageManager.MATCH_DEFAULT_ONLY) != null
 
         dateButton.apply {
             text = crime.date.toString()
@@ -113,6 +166,7 @@ class CrimeDetailFragment : Fragment() {
         return view
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d(logTag, "onViewCreate() called")
         super.onViewCreated(view, savedInstanceState)
@@ -125,6 +179,11 @@ class CrimeDetailFragment : Fragment() {
     private fun updateUI() {
         titleField.setText(crime.title)
         dateButton.text = crime.date.toString()
+
+        if(crime.suspect != null) {
+            crimeSuspectButton.text = crime.suspect
+        }
+
         solvedCheckBox.apply {
             isChecked = crime.isSolved
             jumpDrawablesToCurrentState()
